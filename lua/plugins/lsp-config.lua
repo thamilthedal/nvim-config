c00a2f81bb95ce5c61d2f02c4537ab7e7a13d7d4
thamilthedal <57757731+thamilthedal@users.noreply.github.com>
2024-05-06 08:25:30 +0530

    Add files via upload

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "jedi_language_server", "clangd" }
+                ensure_installed = { "lua_ls", "jedi_language_server", "clangd", "texlab" }
             })
         end
     },
@@ -20,6 +20,7 @@ return {
             lspconfig.lua_ls.setup({})
             lspconfig.jedi_language_server.setup({})
             lspconfig.clangd.setup({})
+            lspconfig.texlab.setup({})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
